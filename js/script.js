5bba5875548a068a55d4b8d4509c5c828b662afe
geo-contr <108944427+geo-contr@users.noreply.github.com>
2022-09-18 13:27:52 +0400

    update js files

--- js/script.js
+++ js/script.js
@@ -310,14 +310,68 @@ flexSwitchCheckDefault.addEventListener("click", () => {
 
 
 // dark mode
-flexSwitchCheckDefault.addEventListener("click", () => {
-	document.body.classList.toggle("dark-theme");
+
+// ეს არის, როცა referesh-ის დროს ისევ light mode-ში ბრუნდება
+// flexSwitchCheckDefault.addEventListener("click", () => {
+// 	document.body.classList.toggle("dark-theme");
+// 	// for input search
+// 	$(".forSearch").toggleClass("forSearch-dark");
+// 	$(".search-item").toggleClass("search-item-dark");
+// 	// for section-overlay
+// 	// $(".section-overlay").toggleClass("section-overlay-show");
+// })
+
+
+// check for saved 'darkMode' and 'darkModeToggle' in localStorage
+  let darkMode = localStorage.getItem('dark-theme');
+
+  const darkModeToggle = document.querySelector('#flexSwitchCheckDefault');
+   
+  const enableDarkMode = () => {
+  	// 1. Add the class to the body
+	document.body.classList.add('dark-theme');
+	// 2. Update darkMode in localStorage
+	localStorage.setItem('dark-theme', 'enabled');
+	// 3. toggle the checkbox
+	darkModeToggle.setAttribute('checked', true);
+	label.innerText = "Disable high contrast";
+
 	// for input search
-	$(".forSearch").toggleClass("forSearch-dark");
-	$(".search-item").toggleClass("search-item-dark");
-	// for section-overlay
-	// $(".section-overlay").toggleClass("section-overlay-show");
-})
+	$(".forSearch").addClass("forSearch-dark");
+    $(".search-item").addClass("search-item-dark");
+  }
+
+  const disableDarkMode = () => {
+    // 1. Remove the class from the body
+    document.body.classList.remove('dark-theme');
+    // 2. Update darkMode and toggle in localStorage 
+    localStorage.setItem('dark-theme', null);
+
+    // for input search
+    $(".forSearch").removeClass("forSearch-dark");
+    $(".search-item").removeClass("search-item-dark");
+  }
+  
+  // If the user already visited and enabled darkMode
+  // start things off with it on
+  if (darkMode === 'enabled') {
+    enableDarkMode();
+  }
+
+  // When someone clicks the button
+  darkModeToggle.addEventListener('click', () => {
+    // get their darkMode setting
+    darkMode = localStorage.getItem('dark-theme');
+    
+    // if it not current enabled, enable it
+    if (darkMode !== 'enabled') {
+      enableDarkMode();
+    // if it has been enabled, turn it off  
+    } else {  
+      disableDarkMode(); 
+    }
+  });
+
 // end of dark mode
 
     
