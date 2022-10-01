d7c9b13b5d35161feac40710d06b3eb1e596cb76
geo-contr <108944427+geo-contr@users.noreply.github.com>
2022-10-01 13:33:57 +0400

    update js files

--- js/slider-1.js
+++ js/slider-1.js
@@ -8,12 +8,78 @@ $(document).ready(function(){
         'img/8810632282142.webp',    // index 1 --> images[1]
         'img/8849829625886.webp'    // index 3 --> images[3]
     ];
+    const title2 = [
+    'LES BEIGES',    // index 0 --> images[0]
+    'LE LIFT CRÈME'    // index 2 --> images[2]
+    ];
+    const title3 = [
+        'LA CRÈME MAIN',    // index 1 --> images[1]
+        'PARIS-PARIS'    // index 3 --> images[3]
+    ];
+    const chracter2 = [
+    'Bronzing Cream',    // index 0 --> images[0]
+    'Smooths – Firms'    // index 2 --> images[2]
+    ];
+    const chracter3 = [
+        'Nourish - Soften - Brighten',    // index 1 --> images[1]
+        'les eaux'    // index 3 --> images[3]
+    ];
+    const price2 = [
+    '$50',    // index 0 --> images[0]
+    '$165'    // index 2 --> images[2]
+    ];
+    const price3 = [
+        '$50',    // index 1 --> images[1]
+        '$140'    // index 3 --> images[3]
+    ];
+    const firstTitle = 0;
+    const lastTitle = title2.length -1;
+    let currentTitle = 0;
+
+    const firstChracter = 0;
+    const lastChracter = chracter.length -1;
+    let currentChracter = 0;
+
+    const firstPrice = 0;
+    const lastPrice = price.length -1;
+    let currentPrice = 0;
+
     const firstImage = 0;
     const lastImage = images2.length -1;
     let currentImage = 0;
     // next
     const nextBtn = document.getElementById('next1');
     nextBtn.addEventListener('click',()=>{
+
+            // get price tag
+            const priceTag2 = document.getElementById('price2');
+            const priceTag3 = document.getElementById('price3');
+            currentPrice++;
+            if(currentPrice >= lastPrice){
+                currentPrice = 1;
+            }
+            priceTag2.innerText = price2[currentPrice];
+            priceTag3.innerText = price3[currentPrice];
+
+            // get chracter tag
+            const chracterTag2 = document.getElementById('chracter2');
+            const chracterTag3 = document.getElementById('chracter3');
+            currentChracter++;
+            if(currentChracter >= lastChracter){
+                currentChracter = 1;
+            }
+            chracterTag2.innerText = chracter2[currentChracter];
+            chracterTag3.innerText = chracter3[currentChracter];
+
+            // get title tag
+            const titleTag2 = document.getElementById('title2');
+            const titleTag3 = document.getElementById('title3');
+            currentTitle++;
+            if(currentTitle >= lastTitle){
+                currentTitle = 1;
+            }
+            titleTag2.innerText = title2[currentTitle];
+            titleTag3.innerText = title3[currentTitle];
 
             // get image tag
             const imageTag2 = document.getElementById('image2');
@@ -28,8 +94,38 @@ $(document).ready(function(){
 
     });
     // prev
-    const preBtn = document.getElementById('prev1');
+    const preBtn = document.getElementById('prev3');
     preBtn.addEventListener('click',()=>{
+
+        // get price tag
+        const priceTag2 = document.getElementById('price2');
+        const priceTag3 = document.getElementById('price3');
+        currentPrice--;
+        if(currentPrice <= firstPrice){
+            currentPrice = 0;
+        }
+        priceTag2.innerText = price2[currentPrice];
+        priceTag3.innerText = price3[currentPrice];
+
+        // get chracter tag
+        const chracterTag2 = document.getElementById('chracter2');
+        const chracterTag3 = document.getElementById('chracter3');
+        currentChracter--;
+        if(currentChracter <= firstChracter){
+            currentChracter = 0;
+        }
+        chracterTag2.innerText = chracter2[currentChracter];
+        chracterTag3.innerText = chracter3[currentChracter];
+
+        // get title tag
+        const titleTag2 = document.getElementById('title2');
+        const titleTag3 = document.getElementById('title3');
+        currentTitle--;
+        if(currentTitle <= firstTitle){
+            currentTitle = 0;
+        }
+        titleTag2.innerText = title2[currentTitle];
+        titleTag3.innerText = title3[currentTitle];
 
         // get image tag
         const imageTag2 = document.getElementById('image2');
@@ -52,9 +148,9 @@ $(document).ready(function(){
         $(".st0-2").addClass("thick");
         $(".st0-3").addClass("thin");
     });
-    $
+   
 
-    (".prev1").click(function() {
+    $(".prev3").click(function() {
         $(".merch-item").removeClass("active");
         $(".st0-2").removeClass("thick");
         $(".st0-3").removeClass("thin");
--- js/slider.js
+++ js/slider.js
@@ -7,12 +7,79 @@ const images1 = [
     'img/8810632282142.webp',    // index 1 --> images[1]
     'img/8849829625886.webp'    // index 3 --> images[3]
 ];
+const title = [
+    'LES BEIGES',    // index 0 --> images[0]
+    'LE LIFT CRÈME'    // index 2 --> images[2]
+];
+const title1 = [
+    'LA CRÈME MAIN',    // index 1 --> images[1]
+    'PARIS-PARIS'    // index 3 --> images[3]
+];
+const chracter = [
+    'Bronzing Cream',    // index 0 --> images[0]
+    'Smooths – Firms'    // index 2 --> images[2]
+];
+const chracter1 = [
+    'Nourish - Soften - Brighten',    // index 1 --> images[1]
+    'les eaux'    // index 3 --> images[3]
+];
+const price = [
+    '$50',    // index 0 --> images[0]
+    '$165'    // index 2 --> images[2]
+];
+const price1 = [
+    '$50',    // index 1 --> images[1]
+    '$140'    // index 3 --> images[3]
+];
+
+const firstTitle = 0;
+const lastTitle = title.length -1;
+let currentTitle = 0;
+
+const firstChracter = 0;
+const lastChracter = chracter.length -1;
+let currentChracter = 0;
+
+const firstPrice = 0;
+const lastPrice = price.length -1;
+let currentPrice = 0;
+
 const firstImage = 0;
 const lastImage = images.length -1;
 let currentImage = 0;
 // next
 const nextBtn = document.getElementById('next');
 nextBtn.addEventListener('click',()=>{
+
+        // get price tag
+        const priceTag = document.getElementById('price');
+        const priceTag1 = document.getElementById('price1');
+        currentPrice++;
+        if(currentPrice >= lastPrice){
+            currentPrice = 1;
+        }
+        priceTag.innerText = price[currentPrice];
+        priceTag1.innerText = price1[currentPrice];
+
+        // get chracter tag
+        const chracterTag = document.getElementById('chracter');
+        const chracterTag1 = document.getElementById('chracter1');
+        currentChracter++;
+        if(currentChracter >= lastChracter){
+            currentChracter = 1;
+        }
+        chracterTag.innerText = chracter[currentChracter];
+        chracterTag1.innerText = chracter1[currentChracter];
+
+        // get title tag
+        const titleTag = document.getElementById('title');
+        const titleTag1 = document.getElementById('title1');
+        currentTitle++;
+        if(currentTitle >= lastTitle){
+            currentTitle = 1;
+        }
+        titleTag.innerText = title[currentTitle];
+        titleTag1.innerText = title1[currentTitle];
 
         // get image tag
         const imageTag = document.getElementById('image');
@@ -27,8 +94,37 @@ nextBtn.addEventListener('click',()=>{
 
 });
 // prev
-const preBtn = document.getElementById('prev');
+const preBtn = document.getElementById('prev2');
 preBtn.addEventListener('click',()=>{
+
+    // get price tag
+    const priceTag = document.getElementById('price');
+    const priceTag1 = document.getElementById('price1');
+    currentPrice--;
+    if(currentPrice >= firstPrice){
+        currentPrice = 0;
+    }
+    priceTag.innerText = price[currentPrice];
+    priceTag1.innerText = price1[currentPrice];
+
+    const chracterTag = document.getElementById('chracter');
+    const chracterTag1 = document.getElementById('chracter1');
+    currentChracter--;
+    if(currentChracter <= firstChracter){
+        currentChracter = 0;
+    }
+    chracterTag.innerText = chracter[currentChracter];
+    chracterTag1.innerText = chracter1[currentChracter];
+
+    // get title tag
+    const titleTag = document.getElementById('title');
+    const titleTag1 = document.getElementById('title1');
+    currentTitle--;
+    if(currentTitle <= firstTitle){
+        currentTitle = 0;
+    }
+    titleTag.innerText = title[currentTitle];
+    titleTag1.innerText = title1[currentTitle];
 
     // get image tag
     const imageTag = document.getElementById('image');
@@ -43,8 +139,6 @@ preBtn.addEventListener('click',()=>{
 
 });
 
-
-
 // this is mine
 $(".next").click(function() {
     $(".merch-item").addClass("active");
@@ -53,7 +147,7 @@ $(".next").click(function() {
 });
 $
 
-(".prev").click(function() {
+(".prev2").click(function() {
     $(".merch-item").removeClass("active");
     $(".st0-2").removeClass("thick");
     $(".st0-3").removeClass("thin");
