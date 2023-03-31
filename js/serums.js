5bd3595ece70f355acc84a65d800db95568ef80b
geo-contr <108944427+geo-contr@users.noreply.github.com>
2023-03-31 20:02:28 +0400

    update serums.js

--- js/serums.js
+++ js/serums.js
@@ -293,3 +293,27 @@ slider1.addEventListener('touchend', () => {
 //   });
 // });
 // end of with slick slider
+
+
+
+// animation on scroll
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 0;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+// end of animation on scroll
